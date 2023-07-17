578e8b34fdbdbf89a39fb8e01400e4b5e4f6f9d9
therealnv6 <devrawr@protonmail.com>
2023-07-17 19:09:13 +0200

    add .gitignore, remove .luarc

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -18,4 +18,6 @@ vim.g.mapleader = " "
 require("plugins.plugins")
 require("options")
 
-local config = require("util").load_config()
+-- call this as last to ensure everything is loaded.
+-- probably not required for most things, but just in case.
+require("util").load_config()
