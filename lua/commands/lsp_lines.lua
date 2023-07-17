107dcd6d60f916cd37925f5c9cba0ca0e0d11702
therealnv6 <devrawr@protonmail.com>
2023-07-18 01:46:39 +0200

    reformat a couple of things

--- lua/commands/lsp_lines.lua
+++ lua/commands/lsp_lines.lua
@@ -1,18 +1,18 @@
 vim.api.nvim_create_user_command("LspLinesToggle", function()
-	local lsp_lines = require("lsp_lines")
-	local diagnostic = require("vim.diagnostic")
+  local lsp_lines = require("lsp_lines")
+  local diagnostic = require("vim.diagnostic")
 
-	lsp_lines.toggle()
+  lsp_lines.toggle()
 
-	local virtual_text_enabled = diagnostic.config().virtual_text
+  local virtual_text_enabled = diagnostic.config().virtual_text
 
-	if virtual_text_enabled then
-		diagnostic.config({
-			virtual_text = false,
-		})
-	else
-		diagnostic.config({
-			virtual_text = true,
-		})
-	end
+  if virtual_text_enabled then
+    diagnostic.config({
+      virtual_text = false,
+    })
+  else
+    diagnostic.config({
+      virtual_text = true,
+    })
+  end
 end, {})
--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,6 @@ opt.autoindent = true
 opt.expandtab = true
 opt.fillchars = "vert: ,eob: "
 
-vim.cmd [[set nu]]
-vim.cmd [[set clipboard+=unnamedplus]]
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+vim.cmd([[set nu]])
+vim.cmd([[set clipboard+=unnamedplus]])
+vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
