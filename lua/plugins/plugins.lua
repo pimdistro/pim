7984932b958cb62cad18f00356de4b78be1a3633
therealnv6 <devrawr@protonmail.com>
2023-07-18 20:11:00 +0200

    use popup menu for first time startup

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -42,6 +42,40 @@ require("lazy").setup({
   "lukas-reineke/lsp-format.nvim",
   "nvim-lua/plenary.nvim",
 
+  "rcarriga/nvim-notify",
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+
+  {
+    "MunifTanjim/nui.nvim",
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+      })
+    end,
+  },
+
   require("plugins.config.cmp"),
   require("custom.plugins"),
 })
